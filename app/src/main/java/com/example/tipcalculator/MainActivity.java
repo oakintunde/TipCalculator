8549cf5a70c5de2c1d1092fabadbf8bab121572f
oakintunde <144389474+oakintunde@users.noreply.github.com>
2025-01-31 15:36:11 -0800

    Add folder to res named layout-land

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -9,6 +9,8 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
 
+import java.text.NumberFormat;
+
 public class MainActivity extends AppCompatActivity
                 implements TextView.OnEditorActionListener, View.OnClickListener {
 
@@ -19,6 +21,8 @@ public class MainActivity extends AppCompatActivity
     private Button percentUpButton;
     private TextView tipTextView;
     private TextView totalTextView;
+    private Object tipPercent;
+    private String billAmountString;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,16 +38,52 @@ public class MainActivity extends AppCompatActivity
         totalTextView = findViewById(R.id.totalTextView);
 
         //set the listeners
-        
+        billAmountEditText.setOnEditorActionListener(this);
+        percentDownButton.setOnClickListener(this);
+        percentUpButton.setOnClickListener(this);
+
+    }
+
+    public void calculateAndDisplay(){
+        //get the bill amount
+        billAmountString = billAmountEditText.getText().toString();
+        float billAmount ;
+        if (billAmountString.equals("")) {
+            billAmount = 0;
+        }
+        else {
+            billAmount = Float.parseFloat(billAmountString);
+        }
+        //calculate tip and total
+        float tipAmount = billAmount + tipPercent;
+        float totalAmount = billAmount + tipAmount;
+
+        //display the result
+        NumberFormat currency = NumberFormat.getCurrencyInstance();
+        tipTextView.setText(currency.format(tipAmount));
+        totalTextView.setText(currency.format(totalAmount));
+
+        NumberFormat percent = NumberFormat.getPercentInstance();
+        percentTextView.setText(percent.format(tipPercent));
     }
 
     @Override
     public void onClick(View v) {
-
+        switch (v.getId()) {
+            case R.id.percentDownButton:
+                tipPercent = tipPercent - 0.01f ;
+                calculateAndDisplay();
+                break;
+            case R.id.percentUpButton:
+                tipPercent = tipPercent + 0.01f;
+                calculateAndDisplay();
+                break;
+        }
     }
 
     @Override
     public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        calculateAndDisplay();
         return false;
     }
 }
