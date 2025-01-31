c85f252e10eb248673d7212bb890c1d1150d2a4a
oakintunde <144389474+oakintunde@users.noreply.github.com>
2025-01-31 11:00:55 -0800

    Add UI controls references

--- app/src/main/java/com/example/tipcalculator/MainActivity.java
+++ app/src/main/java/com/example/tipcalculator/MainActivity.java
@@ -3,12 +3,41 @@ package com.example.tipcalculator;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity
+                implements TextView.OnEditorActionListener, View.OnClickListener {
+
+    //define variables for the user interface controls
+    private EditText billAmountEditText;
+    private TextView percentTextView;
+    private Button percentDownButton;
+    private Button percentUpButton;
+    private TextView tipTextView;
+    private TextView totalTextView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        //get references to the UI controls
+        billAmountEditText = findViewById(R.id.billAmountEditText);
+        percentTextView = findViewById(R.id.percentTextView);
+        
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
+
+    @Override
+    public boolean onEditorAction(TextView v, int actionId, KeyEvent event) {
+        return false;
     }
 }
